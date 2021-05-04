7525fa85f64725b730a2d5fdab249b7020e7160c
vietnguyen270699 <65987730+vietnguyen270699@users.noreply.github.com>
2021-05-04 18:50:22 +0700

    fix key db

--- src/main/java/com/example/bookstoreproject/entity/UserEntity.java
+++ src/main/java/com/example/bookstoreproject/entity/UserEntity.java
@@ -33,13 +33,105 @@ public class UserEntity extends BaseEntity {
 	
 	@OneToMany(mappedBy = "userEntity")
 	private List<FeedbackEntity> feedback;
-	
-	@OneToMany(mappedBy = "userEntity")
-	private List<BookEntity> book ;
 
 	
 	public UserEntity() {
 		super();
+	}
+
+
+	public UserEntity(Long id, Date createDate, String createBy, Date modifyDate, String modifyBy, String email,
+			String password, int phone, String address, String resetPasswordToken, List<RoleEntity> roles,
+			List<BillEntity> bill, List<FeedbackEntity> feedback) {
+		super(id, createDate, createBy, modifyDate, modifyBy);
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		this.address = address;
+		this.resetPasswordToken = resetPasswordToken;
+		this.roles = roles;
+		this.bill = bill;
+		this.feedback = feedback;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public int getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getResetPasswordToken() {
+		return resetPasswordToken;
+	}
+
+
+	public void setResetPasswordToken(String resetPasswordToken) {
+		this.resetPasswordToken = resetPasswordToken;
+	}
+
+
+	public List<RoleEntity> getRoles() {
+		return roles;
+	}
+
+
+	public void setRoles(List<RoleEntity> roles) {
+		this.roles = roles;
+	}
+
+
+	public List<BillEntity> getBill() {
+		return bill;
+	}
+
+
+	public void setBill(List<BillEntity> bill) {
+		this.bill = bill;
+	}
+
+
+	public List<FeedbackEntity> getFeedback() {
+		return feedback;
+	}
+
+
+	public void setFeedback(List<FeedbackEntity> feedback) {
+		this.feedback = feedback;
 	}
 
 	
