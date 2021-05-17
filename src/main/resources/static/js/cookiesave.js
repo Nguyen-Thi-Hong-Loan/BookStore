9639054d1c7c2b4c6bab364d625b694fdeb3b823
vietnguyen270699 <65987730+vietnguyen270699@users.noreply.github.com>
2021-05-17 20:27:56 +0700

    add function admin demo

--- src/main/resources/static/js/cookiesave.js
+++ src/main/resources/static/js/cookiesave.js
@@ -13,6 +13,52 @@ $(document).ready(function() {
 		})
 	})
 })
+$(document).ready(function() {
+	$(".btn-addcart").click(function() {
+		var id = $(this).closest("div").attr("data-id");
+		$.ajax({
+			url: "/bookstore/shopList/addcart/" + id,
+			success: function(response) {
+					$("#cart-count").html(response[0]);
+					alert("Đã thêm thành công: số lượng sản phẩm là " + response[0] + " tổng tiền: " + response[1] )
+				
+			}
+		})
+	})
+})
+$(document).ready(function() {
+	$(".btn-down").click(function() {
+		var id = $(this).closest("div").attr("data-id");
+		var itemcount = "#item-count-";
+		var merger =  itemcount.concat(id);
+		$.ajax({
+			url: "/bookstore/shopList/cart/downUpdatecart/" + id,
+			success: function(response) {
+				$("#cart-count").html(response[0]);
+				$("#total-price").html(response[1]);
+				$(merger).html(response[2]);
+			alert("Đã giảm thành công: " +  "số lượng sách là " +response[0] + " tổng tiền: "+ response[1])
+			}
+		})
+	})
+})
+$(document).ready(function() {
+	$(".btn-up").click(function() {
+		var id = $(this).closest("div").attr("data-id");
+		var itemcount = "#item-count-";
+		var merger =  itemcount.concat(id);
+		$.ajax({
+			url: "/bookstore/shopList/cart/updatecart/" + id,
+			success: function(response) {
+				$("#cart-count").html(response[0]);
+				$("#total-price").html(response[1]);
+				$(merger).html(response[2]);
+			alert("Đã tăng thành công: " + "số lượng sách là: " +response[0] + " tổng tiền: "+ response[1])
+			}
+		})
+	})
+})
+
 
 
 
