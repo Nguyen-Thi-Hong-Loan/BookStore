bf1f9bef3437b2445d6b49a721e7398b6b8c4bc7
{Nguyen-Thi-Hong-Loan} <66268096+Nguyen-Thi-Hong-Loan@users.noreply.github.com>
2021-05-21 20:44:51 +0700

    update

--- src/main/java/com/example/bookstoreproject/services/BillService.java
+++ src/main/java/com/example/bookstoreproject/services/BillService.java
@@ -4,9 +4,34 @@ import com.example.bookstoreproject.entity.BillEntity;
 import com.example.bookstoreproject.entity.UserEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public interface BillService {
 
     BillEntity findByUserEntity(UserEntity userEntity);
+
+    void deleteAll();
+
+    void deleteAll(List<BillEntity> entities);
+
+    void delete(BillEntity entity);
+
+    void deleteById(Long id);
+
+    long count();
+
+    List<BillEntity> findAllById(List<Long> ids);
+
+    Iterable<BillEntity> findAll();
+
+    boolean existsById(Long id);
+
+    Optional<BillEntity> findById(Long id);
+
+    List<BillEntity> saveAll(List<BillEntity> entities);
+
+    BillEntity save(BillEntity entity);
 
 }
--- src/main/java/com/example/bookstoreproject/services/impl/BillEntityServiceImpl.java
+++ src/main/java/com/example/bookstoreproject/services/impl/BillEntityServiceImpl.java
@@ -7,6 +7,9 @@ import com.example.bookstoreproject.services.BillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class BillEntityServiceImpl implements BillService {
     @Autowired
@@ -16,4 +19,60 @@ public class BillEntityServiceImpl implements BillService {
     public BillEntity findByUserEntity(UserEntity userEntity) {
         return billRepository.findByUserEntity(userEntity).orElse(null);
     }
+
+    @Override
+    public BillEntity save(BillEntity entity) {
+        return billRepository.save(entity);
+    }
+
+    @Override
+    public List<BillEntity> saveAll(List<BillEntity> entities) {
+        return (List<BillEntity>) billRepository.saveAll(entities);
+    }
+
+    @Override
+    public Optional<BillEntity> findById(Long id) {
+        return billRepository.findById(id);
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return billRepository.existsById(id);
+    }
+
+    @Override
+    public Iterable<BillEntity> findAll() {
+        return billRepository.findAll();
+    }
+
+    @Override
+    public List<BillEntity> findAllById(List<Long> ids) {
+        return (List<BillEntity>) billRepository.findAllById(ids);
+    }
+
+    @Override
+    public long count() {
+        return billRepository.count();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        billRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(BillEntity entity) {
+        billRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteAll(List<BillEntity> entities) {
+        billRepository.deleteAll(entities);
+    }
+
+    @Override
+    public void deleteAll() {
+        billRepository.deleteAll();
+    }
+
 }
