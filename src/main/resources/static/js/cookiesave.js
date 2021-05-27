4edf44ec5a697bb2538da25b5282c7a1bb994396
vietnguyen270699 <65987730+vietnguyen270699@users.noreply.github.com>
2021-05-27 14:31:29 +0700

    edit template

--- src/main/resources/static/js/cookiesave.js
+++ src/main/resources/static/js/cookiesave.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 			success: function(response) {
 				if (response) {
 					alert("Đã cho thích thành công: " + id)
-				}else{
-					alert("Đã tồn tại: " + id )
+				} else {
+					alert("Đã tồn tại: " + id)
 				}
 			}
 		})
@@ -19,9 +19,9 @@ $(document).ready(function() {
 		$.ajax({
 			url: "/bookstore/shopList/addcart/" + id,
 			success: function(response) {
-					$("#cart-count").html(response[0]);
-					alert("Đã thêm thành công: số lượng sản phẩm là " + response[0] + " tổng tiền: " + response[1] +" VNĐ" )
-				
+				$("#cart-count").html(response[0]);
+				alert("Đã thêm thành công: số lượng sản phẩm là " + response[0] + " tổng tiền: " + response[1] + " VNĐ")
+
 			}
 		})
 	})
@@ -30,7 +30,7 @@ $(document).ready(function() {
 	$(".btn-down").click(function() {
 		var id = $(this).closest("div").attr("data-id");
 		var itemcount = "#item-count-";
-		var merger =  itemcount.concat(id);
+		var merger = itemcount.concat(id);
 		$.ajax({
 			url: "/bookstore/shopList/cart/downUpdatecart/" + id,
 			success: function(response) {
@@ -45,14 +45,14 @@ $(document).ready(function() {
 	$(".btn-up").click(function() {
 		var id = $(this).closest("div").attr("data-id");
 		var itemcount = "#item-count-";
-		var merger =  itemcount.concat(id);
+		var merger = itemcount.concat(id);
 		$.ajax({
 			url: "/bookstore/shopList/cart/updatecart/" + id,
 			success: function(response) {
 				$("#cart-count").html(response[0]);
 				$("#total-price").html(response[1]);
 				$(merger).html(response[2]);
-			
+
 			}
 		})
 	})
@@ -69,12 +69,35 @@ $(document).ready(function() {
 			success: function(response) {
 				if (response) {
 					alert("Đã xóa thành công: " + id)
-					  location.reload();
-				}else{
-					alert("Xóa thất bại : " + id )
-					  location.reload();
+					location.reload();
+				} else {
+					alert("Xóa thất bại : " + id)
+					location.reload();
 				}
 			}
 		})
 	})
 })
+
+//var number = document.getElementsById("money").textContent;
+
+/*var number = document.getElementsByClassName("money");
+for (i=0; i< munber.lenght; i++) {
+   document.write(i + "<br >");
+}
+var so =  Number(number);
+so.toLocaleString('vi-VN')
+ document.getElementsById("money").innerHTML = so.toLocaleString('vi-VN');*/
+
+
+//
+  var a = document.getElementsByClassName("money");
+  
+ 
+var i;
+for (i = 0; i < a.length; i++) {
+  let char = a[i].textContent;
+          let b = Number(char);
+         a[i].innerHTML = 		b.toLocaleString('vi-VN');
+
+}
